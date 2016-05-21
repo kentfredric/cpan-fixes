41179b3f8e925c2d9dccf237ef1bdd559f2e0bf9
Kent Fredric <kentfredric@gmail.com>
2016-05-21 18:07:45 +1200

    Add really bad index and really lazy generator

---
+++ mkindex.pl
@@ -0,0 +1,26 @@
+#!perl
+use strict;
+use warnings;
+
+use Path::Tiny qw( path );
+
+my $root = path(__FILE__)->parent;
+
+my $iter = $root->iterator({
+  recurse => 1,
+  follow_symlinks => 0,
+});
+
+while ( my $path = $iter->() ) {
+  next if $path->basename eq 'mkindex.pl';
+  next if $root->child('.git')->subsumes($path);
+  next if $path->is_dir;
+  my $relpath =$path->relative($root);
+  my $size = $path->stat->size;
+
+  my $sz = sprintf "%5s", $size;
+  $sz =~ s/ /&nbsp;/g;
+  printf "%s - <a href=\"./%s\">%s</a>\n", $sz, $relpath, $relpath;
+}
+
+
